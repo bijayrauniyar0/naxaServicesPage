03d19a41d5b22998b2dd84c33cdfa400392e72a8
Bijay <Bijay Rauniyar>
2024-06-24 13:40:17 +0545

    navbar added

---
+++ src/subcomponents/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import './Navbar.css'
+import logo1 from '../assets/logo1.svg'
+import logo2 from '../assets/logo2.svg'
+import logo3 from '../assets/logo3.png'
+import logo4 from '../assets/logo4.svg'
+import logo5 from '../assets/logo5.svg'
+import logo6 from '../assets/logo6.svg'
+import logo7 from '../assets/logo7.svg'
+
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='nav'>
+            <div className='nav-logo'>
+                <img src={logo} alt='logo' />
+            </div>
+            <div className='nav-links'>
+                <ul>
+                    <li><a href='#'>Services</a></li>
+                    <li><span>Portfolio <i class="fa-solid fa-caret-down"></i></span>
+                        <ul>
+                            <li><a href="">General</a></li>
+                            <li><a href="">International</a></li>
+                        </ul>
+                    </li>
+                    <li><span>Company <i class="fa-solid fa-caret-down"></i></span>
+                        <ul>
+                            <li><a href=""><img src={logo1} alt="" />About Us</a></li>
+                            <li><a href=""><img src={logo2} alt="" />Team</a></li>
+                            <li><a href=""><img src={logo3} alt="" />Work With Us</a></li>
+                            <li><a href=""><img src={logo4} alt="" />Awards and Achievements</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='#'>GeoAI</a></li>
+                    <li><span>Events & Media <i class="fa-solid fa-caret-down"></i></span> 
+                        <ul>
+                            <li><a href=""><img src={logo5} alt="" /> Events</a></li>
+                            <li><a href=""><img src={logo6} alt="" /> Media</a></li>
+                            <li><a href=""><img src={logo7} alt="" /> Publications</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='#'>Blogs</a></li>
+                </ul>
+            </div>
+            <div className="right-nav">
+                <button className='nav-btn'>Let's Talk</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
--- src/subcomponents/TopComponent.jsx
+++ src/subcomponents/TopComponent.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Components.css'
+import Navbar from './Navbar'
 
 const TopComponent = () => {
   return (
     <>
-        <div className='services-top'>
-            <h2>SERVICES</h2>
-            <h1>At <span className="blue-color">NAXA</span>, we work on <span className="blue-color">ideas</span>; ideas that can provide <span className="blue-color">simple solutions</span> to <span className="blue-color">complex problems</span>.</h1>
-            <p>We work as a team to generate, explore, build and validate ideas. We also contextualize innovations around the world to find the best fitting solutions to local problems.</p>
+        <div className='services-head'>
+          <Navbar />
+          <div className='services-top'>
+              <h2>SERVICES</h2>
+              <h1>At <span className="blue-color">NAXA</span>, we work on <span className="blue-color">ideas</span>; ideas that can provide <span className="blue-color">simple solutions</span> to <span className="blue-color">complex problems</span>.</h1>
+              <p>We work as a team to generate, explore, build and validate ideas. We also contextualize innovations around the world to find the best fitting solutions to local problems.</p>
+          </div>
         </div>
     </>
   )
